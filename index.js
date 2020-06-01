5d671e36ecc9bc2951b0e4d7c1fb9c8b0ff00712
Bruno Hradec <bhradec@gmail.com>
2020-06-01 14:50:51 +0200

    dodana validacija ip adrese

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+var networkAddressInput = document.getElementById("networkAddress");
 var subnetInputTable = document.getElementById("subnetInputTable");
 var numOfSubnetsInput = document.getElementById("numOfSubnets");
 var calculateButton = document.getElementById("calculate");
 var currentRow = 0;
 
+/**
+ * Adds a row to the subnet input table.
+ * A row contains subnet name cell and a host number cell.
+ */
 function addInputTableRow() {
     let tableRow = subnetInputTable.insertRow();
     let tableDataName = tableRow.insertCell();
@@ -25,19 +30,58 @@ function addInputTableRow() {
     tableDataNumOfHosts.appendChild(numOfHostsInput);
 }
 
+/**
+ * Removes a row from the subnet input table
+ */
 function removeInputTableRow() {
     subnetInputTable.deleteRow(currentRow);
     currentRow--;
 }
 
+/**
+ * Generates subnet input table with the given number of rows.
+ * @param {number} numOfSubnets Number od rows in the subnet input table.
+ */
 function generateSubnetInputTable(numOfSubnets) {
     for (i = 0; i < numOfSubnets; i++) {
         addInputTableRow();        
     }
 }
 
-/* Generates input table for the default number of subnets */
-generateSubnetInputTable(numOfSubnetsInput.value);
+/**
+ * Validates an ipv4 address.
+ * @param {string} ipAddress  Ipv4 address to validate.
+ * @return {boolean} Returns true if ipv4 address is valid, false if not.
+ */
+function validateIpAddress(ipAddress) {
+    let ipRegex = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/;
+    
+    if (ipRegex.test(ipAddress)) return true  
+    else return false;
+}
+
+/**
+ * Validates an ipv4 address with the network mask suffix.
+ * @param {string} ipAddresCIDR Ipv4 address with the network suffix (ex: 192.168.1.1/24)
+ * @return {boolean} Returns true if ipv4 address and suffix are valid, false if not. 
+ */
+function validateIpAddressCIDR(ipAddresCIDR) {
+    let ipAddress = ipAddresCIDR.split("/")[0];
+    let suffix = Number(ipAddresCIDR.split("/")[1]);
+
+    console.log("IPADDR: " + ipAddress);
+    console.log("SUFF: " + suffix);
+
+    if (validateIpAddress(ipAddress) == false) {
+        return false;
+    }
+
+    if (suffix < 0 || suffix > 32) {
+        return false;
+    }
+
+    return true;
+}
 
 /* Adds or removes a table row on subnet number change */
 numOfSubnetsInput.onchange = () => {
@@ -51,6 +95,13 @@ numOfSubnetsInput.onchange = () => {
 }
 
 calculateButton.onclick = () => {
+    let networkAddress = networkAddressInput.value;
+
+    if(validateIpAddressCIDR(networkAddress) == false) {
+        let errorSpan = document.getElementById("networkAddressError");
+        errorSpan.innerHTML = "Invalid IPv4 address!";
+    }
+
     let subnetNameInputs = document.getElementsByClassName("subnetNameInput");
     let numOfHostsInputs = document.getElementsByClassName("numOfHostsInput")
 
@@ -66,14 +117,13 @@ calculateButton.onclick = () => {
 
     /* sort subnetInputData by number of hosts descending */
     subnetInputData.sort((a, b) => {
-        if (a.numOfHosts > b.numOfHosts) {
-            return -1;
-        } else {
-            return 1;
-        }
+        if (a.numOfHosts > b.numOfHosts) return -1;
+        else return 1;
     })
 
-    for (const dataobject of subnetInputData) {
-        console.log(dataobject.numOfHosts);
-    }    
+    // caluculateSubnets(subnetInputData, networkAddress);
+
 }
+
+/* Generates input table for the default number of subnets */
+generateSubnetInputTable(numOfSubnetsInput.value);
